087d23fe6ae5996a0ab8466a97b19ffa29df2165
SundlyDev <sundly@live.fr>
2024-10-22 19:59:20 +0200

    Serialize + test postman OK

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -6,7 +6,11 @@ use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 
 
@@ -17,38 +21,52 @@ class UtilisateurController extends AbstractController
 
 public function __construct(
     private EntityManagerInterface $manager,
-    private UtilisateurRepository $utilisateurRepository
+    private UtilisateurRepository $utilisateurRepository,
+    private SerializerInterface $serializer,
+    private UrlGeneratorInterface $urlGenerator,
 ) {
 
 }
 
 
 //METHODE POST
-    #[Route(name: 'new', methods: ['POST'])]
-  public function new(): Response
+    #[Route( methods: ['POST'])]
+  public function new(Request $request): JsonResponse
   {
+ //Création d'un objet utilisateur static en dur avecc de fausses données pour tester l'api
+// $utilisateur = new Utilisateur();
+// $utilisateur->setUsername('testcrud@mail.com');
+// $utilisateur->setPassword('Azerty_123');
+// $utilisateur->setNom('koko');
+// $utilisateur->setPrenom('jean');
 
 
+//Serialiszer transforme un format en un autre format
+$utilisateur = $this->serializer->deserialize($request->getContent(), Utilisateur::class, 'json');
 
-
-    //Création d'un objet utilisateur en dur avecc de fausses données pour tester l'api
-$utilisateur = new Utilisateur();
-$utilisateur->setUsername('testcrud@mail.com');
-$utilisateur->setPassword('Azerty_123');
-$utilisateur->setNom('koko');
-$utilisateur->setPrenom('jean');
-
+        
 
 
 //On met l'objet sur liste d'attente avec persist puis on le push avec flush
 $this->manager->persist($utilisateur);
 $this->manager->flush();
 
+
+$responseData = $this->serializer->serialize($utilisateur, 'json');
+
+        $location = $this->urlGenerator->generate(
+
+            'app_api_utilisateurshow',
+
+            ['id' => $utilisateur->getId()],
+
+            UrlGeneratorInterface::ABSOLUTE_URL,
+        );
+
+
+
 //à stocker en bdd
-return $this->json(
-    ['message' => "utilisateur resource created with {$utilisateur->getId()} id"],
-Response::HTTP_CREATED
-);
+return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
 
   }
 
@@ -61,12 +79,12 @@ Response::HTTP_CREATED
     {
 $utilisateur = $this->utilisateurRepository->findOneBy(['id' => $id]);
         // $utilisateur = Chercher utilisateur avec l'id = 1
-        if(!$utilisateur) {
-            throw new \Exception ("no utilisateur found for {$id} id" );
+        if($utilisateur) {
+            $responseData = $this->serializer->serialize($utilisateur, 'json');
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
         }
-return $this->json(
-    ['Message' => "utilisateur was found {$utilisateur->getNom()} for {$utilisateur->getId()} id"]
-);
+return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+
     }
 
 
@@ -74,16 +92,24 @@ return $this->json(
 
 //METHODE PUT
     #[Route('/{id}', name: 'edit', methods: ['PUT'])]
-    public function edit(int $id): Response
+    public function edit(int $id, Request $request): JsonResponse
     {
         $utilisateur = $this->utilisateurRepository->findOneBy(['id' => $id]);
-        if (!$utilisateur) {
-            throw new \Exception("no utilisateur found for {$id} id");
-        }
-$utilisateur->setNom('utilisateur name updated');
-        $this->manager->flush();
 
-return $this->redirectToRoute('app_api_utilisateur_show', ['id'=> $utilisateur->getId()]);
+
+        if ($utilisateur) {
+           $utilisateur = $this->serializer->deserialize(
+            $request->getContent(),
+            Utilisateur::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE =>$utilisateur]
+           );
+
+        
+return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+
     }
 
 
@@ -100,6 +126,9 @@ return $this->redirectToRoute('app_api_utilisateur_show', ['id'=> $utilisateur->
         if (!$utilisateur) {
             throw new \Exception("no utilisateur found for {$id} id");
         }
+
+        $this->manager->remove($utilisateur); //S'il ne trouve pas, il supprime l'information
+        $this->manager->flush();
         return $this->json(['Message' => 'utilisateur resource deleted'], Response::HTTP_NO_CONTENT);
     }
     }
