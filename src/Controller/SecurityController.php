ce9c5c78c2624688e008fed8d3a8c335d39417eb
SundlyDev <sundly@live.fr>
2024-10-24 11:44:13 +0200

    Ajout system AUTHENTIFICATION + test postman OK

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,8 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 
+
+
+
+
+//Système d'inscription
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
@@ -31,6 +37,29 @@ public function __construct(private EntityManagerInterface $manager, private ser
 
         $this->manager->persist($user);
         $this->manager->flush();
-        return new JsonResponse(['user' => $user->getUserIdentifier(), 'apiToken' => $user->getApiToken(), 'Roles' => $user->getRoles()], Response::HTTP_CREATED);
+        return new JsonResponse(['user' => $user->getUserIdentifier(), 'apiToken' => $user->getApiToken(), 'roles' => $user->getRoles()], Response::HTTP_CREATED);
+    
+}
+
+
+
+//system d'authentification
+
+#[Route('/login', name: 'login', methods: 'POST')]
+    public function login(#[CurrentUser] ?User $user): JsonResponse //Va chercher l'utilisateur en base de données
+    {
+        if (null === $user) {
+            return new JsonResponse(['message' => 'Missing credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'user'  => $user->getUserIdentifier(),
+            'apiToken' => $user->getApiToken(),
+            'roles' => $user->getRoles(),
+        ]);
     }
 }
+
+
+
+
